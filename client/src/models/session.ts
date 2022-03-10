76fe00df347ba979b2e6c3f89f8136ba02fbe2fa
jeremy segarra <jeremysegarra92@gmail.com>
2022-03-10 15:09:18 -0500

    added error message

--- client/src/models/session.ts
+++ client/src/models/session.ts
@@ -3,6 +3,7 @@ import { reactive } from "vue";
 
 // import { User, list } from '../models/user'
 import * as users from "../models/user";
+import { useMessages } from "./messages";
 
 const session = reactive({
   //typescript is a non null language so null as users.User cannot convert
@@ -11,21 +12,34 @@ const session = reactive({
 
 export async function Login(handle: String, password: String) {
   const user = users.list.find((u) => u.handle === handle);
+  const messages = useMessages();
 
-  if (!user) {
-    //if user is null or undefined we would never reach session.user = user
-    throw { message: "User not found" };
+  try {
+    if (!user) {
+      //if user is null or undefined we would never reach session.user = user
+      throw { message: "User not found" };
+    }
+    if (user.password !== password) {
+      throw { message: "Incorrect password" };
+    }
+    messages.notifications.push({
+      type: "success",
+      message: `Welcome Back ${user.firstName}!`,
+    });
+    session.user = user;
+    router.push("/messages");
+  } catch (error: any) {
+    messages.notifications.push({
+      type: "danger",
+      message: error.message,
+    });
+    console.table(messages.notifications);
   }
-  if (user.password !== password) {
-    throw { message: "Incorrect password" };
-  }
-
-  session.user = user;
-  router.push("/messages");
 }
 
 export function Logout() {
   session.user = null;
+  router.push("/login");
 }
 
 export default session;
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -15,7 +15,12 @@ const routes: RouteRecordRaw[] = [
   { path: "/contact", component: Generic, props: { title: "Contact Page!" } },
   { path: "/login", component: Login },
   { path: "/signup", component: Generic, props: { title: "SignUp Page!" } },
-  { path: "/messages", component: () => import("../pages/Wall.vue") },
+  { path: "/wall", component: () => import("../pages/Wall.vue") },
+  {
+    path: "/hidden",
+    component: Generic,
+    props: { title: "You reached the hidden page" },
+  },
 ];
 
 // 3. Create the router instance and pass the `routes` option
@@ -30,8 +35,14 @@ const router = createRouter({
 
 router.beforeEach((to, from) => {
   //list of paths that require login!
-  if (["/messages", "/wall", "/feed"].includes(to.path)) {
+  console.log({ to });
+  const protectedUrls = ["/messages", "/wall", "/feed", "/hidden"];
+  console.table({ protectedUrls });
+
+  if (protectedUrls.includes(to.path)) {
     if (!session.user) {
+      console.log("requires login");
+
       return "/login";
     }
   }
