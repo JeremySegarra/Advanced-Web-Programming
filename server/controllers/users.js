6e8cadb4dbf724904a7a4db90c13ccf476dab2f1
JeremySegarra <93934254+JeremySegarra@users.noreply.github.com>
2022-03-24 19:19:12 +0000

    Deploying to node-deploy from @ JeremySegarra/Advanced-Web-Programming@8655b9de60aa4abc748c614fcf376cbca4e23e73 🚀

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,15 +2,22 @@ const express = require("express"); //importing express
 const app = express.Router(); //the router object is made to be used as middleware works very similar to express()
 
 const userModel = require("../models/user");
+const CREATED_STATUS = 201;
 
 app
   .get("/", (req, res) => {
-    res.send(user.list);
+    res.send(userModel.list);
   })
   .get("/:id", (req, res) => {
     const user = userModel.get(req.params.id); //we exported this function, model should know what database its accessing, models do not know what plateform we are using
     //controller should know its working with express but express does not know what database
     res.send(user);
+  })
+  .post("/", (req, res) => {
+    const user = userModel.create(req.body);
+    res.status(CREATED_STATUS).send(user);
+    //Your controller is never supposed to know what database you are using
+    //always avoid adding magic numbers to your code, i.e means something to the computer
   });
 
 module.exports = app; //This is how common js exports items
