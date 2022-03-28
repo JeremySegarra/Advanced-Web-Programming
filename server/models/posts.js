2e90eda7348c6e7d23c6e06f6e0cdb589b568ed9
JeremySegarra <93934254+JeremySegarra@users.noreply.github.com>
2022-03-28 20:02:34 +0000

    Deploying to node-deploy from @ JeremySegarra/Advanced-Web-Programming@259db8be79ac29448bf69198c6d6340826990502 🚀

--- server/models/posts.js
+++ server/models/posts.js
@@ -1,58 +1,64 @@
+const userModel = require("./user");
+
 let highestId = 3;
 
 const list = [
   {
-    firstName: "John",
-    lastName: "Doe",
-    handle: "johndoe",
-    password: "password",
-    email: "jhon@doe.com",
-    pic: "https://randomuser.me/api/portraits/men/1.jpg",
-    id: 1,
+    src: "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-1120x560,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg",
+    caption:
+      "Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two",
+    owner: 1,
+    likes: [],
+    comments: [],
+    isPublic: true,
   },
+
   {
-    firstName: "Vladimir",
-    lastName: "Putin",
-    handle: "russian_dictator",
-    password: "long table",
-    email: "jhon@doe.com",
-    pic: "https://randomuser.me/api/portraits/men/2.jpg",
-    id: 2,
+    src: "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-1120x560,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg",
+    caption:
+      "Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two",
+    owner: 1,
+    likes: [],
+    comments: [],
+    isPublic: true,
   },
+
   {
-    firstName: "Kamala",
-    lastName: "Harris",
-    handle: "vp",
-    password: "password",
-    email: "kamala@whitehouse.org",
-    pic: "https://randomuser.me/api/portraits/women/3.jpg",
-    id: 3,
+    src: "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-1120x560,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg",
+    caption:
+      "Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two",
+    owner: 1,
+    likes: [],
+    comments: [],
+    isPublic: true,
   },
 ];
 
 function get(id) {
   //so we copy all the properties of the user to the object and we add 1 more property here as undefeined
   return {
-    ...list.find((user) => user.id === parseInt(id)),
+    ...list.find((x) => x.id === parseInt(id), {
+      user: userModel.get(x.owner),
+    }),
     password: undefined,
   };
 }
 
 function remove(id) {
   //findIndex returns the number the actual index not the object like the find() method
-  const index = list.findIndex((user) => user.id === parseInt(id));
-  const user = list.splice(index, 1);
+  const index = list.findIndex((x) => x.id === parseInt(id));
+  const post = list.splice(index, 1);
 
-  return { ...user[0], password: undefined };
+  return { ...post[0], user: userModel.get(post[0].owner) };
 }
-function update(id, newUser) {
+function update(id, newPost) {
   //we want the index because we are updating that exact object
   const index = list.findIndex((user) => user.id === parseInt(id));
-  const oldUser = list[index];
+  const oldPost = list[index];
 
   console.log(list);
   //we find the item in our list, we get the old user object, then we say that item is going to be updated with some of the old user and the new user
-  newUser = list[index] = { ...oldUser, ...newUser };
+  newPost = list[index] = { ...oldUser, ...newUser };
 
   return { ...newUser, password: undefined };
 }
