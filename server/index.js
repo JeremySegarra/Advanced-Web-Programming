259db8be79ac29448bf69198c6d6340826990502
jeremy segarra <jeremysegarra92@gmail.com>
2022-03-28 16:01:46 -0400

    fixed public folder/port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const usersController = require("./controllers/users");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app
 
