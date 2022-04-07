54c3952c31f39a82fc7f8a49c2ec7d36ad3e4d10
JeremySegarra <93934254+JeremySegarra@users.noreply.github.com>
2022-04-07 15:18:08 +0000

    Deploying to node-deploy from @ JeremySegarra/Advanced-Web-Programming@b72919c2005f41020086f784f1d606a582cb1979 🚀

--- server/models/posts.js
+++ server/models/posts.js
@@ -1,89 +1,77 @@
 const userModel = require("./user");
 
-let highestId = 3;
+let hieghstId = 3;
 
 const list = [
   {
-    src: "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-1120x560,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg",
+    src: "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-560x280,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg",
     caption:
       "Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two",
     owner: 1,
     likes: [],
     comments: [],
     isPublic: true,
+    id: 1,
   },
-
   {
-    src: "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-1120x560,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg",
-    caption:
-      "Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two",
+    src: "https://w2.chabad.org/media/images/1187/LUSK11877068.jpg?_i=_nF72F3FCB89E9790A548A28674376BE69",
+    caption: "How Jews in Ukraine Will Celebrate Passover This Year",
     owner: 1,
     likes: [],
     comments: [],
     isPublic: true,
+    id: 2,
   },
-
   {
-    src: "https://media-cldnry.s-nbcnews.com/image/upload/t_focal-1120x560,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg",
-    caption:
-      "Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two",
-    owner: 1,
+    src: "https://w2.chabad.org/media/images/1187/sTyy11875354.jpg?_i=_nF72F3FCB89E9790A548A28674376BE69",
+    caption: "What It Has Been Like to Host Ukraine Refugees",
+    owner: 2,
     likes: [],
     comments: [],
     isPublic: true,
+    id: 3,
   },
 ];
 
+const includeUser = (post) => ({ ...post, user: userModel.get(post.owner) });
+
 function get(id) {
-  //so we copy all the properties of the user to the object and we add 1 more property here as undefeined
-  return {
-    ...list.find((x) => x.id === parseInt(id), {
-      user: userModel.get(x.owner),
-    }),
-    password: undefined,
-  };
+  const post = list.find((x) => x.id === parseInt(id));
+  if (!post) {
+    throw { status: 404, message: "Post not found" };
+  }
+  return includeUser(post);
 }
 
 function remove(id) {
-  //findIndex returns the number the actual index not the object like the find() method
   const index = list.findIndex((x) => x.id === parseInt(id));
   const post = list.splice(index, 1);
 
-  return { ...post[0], user: userModel.get(post[0].owner) };
+  return includeUser(post[0]);
 }
+
 function update(id, newPost) {
-  //we want the index because we are updating that exact object
-  const index = list.findIndex((user) => user.id === parseInt(id));
+  const index = list.findIndex((x) => x.id === parseInt(id));
   const oldPost = list[index];
 
-  console.log(list);
-  //we find the item in our list, we get the old user object, then we say that item is going to be updated with some of the old user and the new user
-  newPost = list[index] = { ...oldUser, ...newUser };
+  newPost = list[index] = { ...oldPost, ...newPost };
 
-  return { ...newUser, password: undefined };
+  //console.log(list);
+
+  return includeUser(newPost);
 }
 
-//dont do it this way just another way if this is put under the other exports it will break so order matters
 module.exports = {
-  create(user) {
-    user.id = ++highestId;
-    //plus signs is pre increment
+  create(post) {
+    post.id = ++hieghstId;
 
-    list.push(user);
-    return { ...user, password: undefined };
+    list.push(post);
+    return includeUser(post);
   },
   remove,
   update,
-  //this made a getter function
   get list() {
-    return list.map((x) => ({ ...x, password: undefined }));
+    return list.map((x) => includeUser(x));
   },
-
-  //putting remove here adds another property to this object and it includes whatever is inside the function it includes it
 };
-
-//we use map once we wrap it in parenthesis it knows its the return object and not the body of the function
-//we fixed this into a function so we can keep updating the list
-
-// module.exports.list = list.map((x) => ({ ...x, password: undefined }));  //this is just a named export .list
-module.exports.get = get; //export our function as a named export
+module.exports.get = get;
--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 let highestId = 3;
 
 const list = [
@@ -45,10 +48,12 @@ function remove(id) {
 
   return { ...user[0], password: undefined };
 }
-function update(id, newUser) {
+async function update(id, newUser) {
   //we want the index because we are updating that exact object
   const index = list.findIndex((user) => user.id === parseInt(id));
   const oldUser = list[index];
+  //update this later professor had to leave
+  newUser.password = await bcrypt.hash(newUser.password, 10);
 
   console.log(list);
   //we find the item in our list, we get the old user object, then we say that item is going to be updated with some of the old user and the new user
@@ -58,16 +63,67 @@ function update(id, newUser) {
 }
 
 //dont do it this way just another way if this is put under the other exports it will break so order matters
+
+async function login(email, password) {
+  const user = list.find((user) => user.email === email);
+  if (!user) {
+    throw { statusCode: 404, message: "User not found" };
+  }
+  //compare returns a PROMISE therefore we await it, it compares both passwords
+  if (!(await bcrypt.compare(password, user.password))) {
+    throw { statusCode: 401, message: "Wrong password" };
+  }
+
+  const data = { ...user, password: undefined };
+  const token = jwt.sign(data, process.env.JWT_SECRET);
+
+  return { ...data, token };
+}
+
+function fromToken(token) {
+  //these two paramteres are two function resolve and reject
+  //we created our own PROMISE we do not need to mark the function as async because we are making our own promise
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(decoded);
+      }
+    });
+  });
+}
+
 module.exports = {
-  create(user) {
+  async create(user) {
     user.id = ++highestId;
     //plus signs is pre increment
 
+    //we are going to hash the password, running bcrypt 10 times in a second nopw whole server will be hung for 1 second everything stops
+    //if a thousand different people are hitting requests at the same time all of a sudden the server will come to a halt
+    //whenever you do something that is long running you do not want to stop everything (node is single threaded) so we use ASYNC programming
+    //We tell the operating system you do it but we are not going to wait you tell me when you re going to be done.
+    //the last parameter of an async function is a function
+    //When you call an async function you get a return value which is a PROMISE that one day the operating system will get back to you and tell you if that function was successful or not and what the return value was.
+    //2 functions are on the PROMISE object and they are .then() and .catch()
+    //.then() will only get executed when it is done and the results are in
+    //The next level is important (async await) if you put async infront of the function you define then that function will always return a PROMISE
+    //when it sees the word await the compiler rewrites it as a PROMISE and puts everything inside of a .then()
+    //we never want magic numbers in our code so we use process.env.SALT_ROUNDS
+    //here we pass it the password and it gives you back the hash with the salt appended, hashSync halts you program it returns the hash
+    //we can only use await on a PROMISE await returns not the PROMISE but the
+
+    //we got an error saltrounds is a string so we need to put a + infront of it to maker it a number
+    user.password = await bcrypt.hash(user.password, +process.env.SALT_ROUNDS);
+    console.log(user);
+    throw { message: "fake error" };
     list.push(user);
     return { ...user, password: undefined };
   },
   remove,
   update,
+  login,
+  fromToken,
   //this made a getter function
   get list() {
     return list.map((x) => ({ ...x, password: undefined }));
